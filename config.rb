20ff78b4909e299f48df92c05e407edcca2aa65c
Sergey Suprunenko <suprunenko.s@gmail.com>
2015-04-07 16:46:31 +0300

    Setup layouts and main files

--- config.rb
+++ config.rb
@@ -1,72 +1,30 @@
-###
-# Compass
-###
+require 'slim'
 
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
+activate :automatic_image_sizes
 
-###
-# Page options, layouts, aliases and proxies
-###
+set :css_dir,    'assets/stylesheets'
+set :js_dir,     'assets/javascripts'
+set :images_dir, 'assets/images'
 
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
+configure :development do
+  activate :livereload
 
-# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
+  compass_config do |config|
+    config.output_style = :expanded
+  end
+end
 
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-set :css_dir, 'stylesheets'
-
-set :js_dir, 'javascripts'
-
-set :images_dir, 'images'
-
-# Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :asset_hash
+  activate :gzip
+  activate :minify_css
+  activate :minify_html
+  activate :minify_javascript
+  activate :relative_assets
 
-  # Minify Javascript on build
-  # activate :minify_javascript
+  set :relative_links, true
 
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  compass_config do |config|
+    config.output_style = :compressed
+  end
 end
